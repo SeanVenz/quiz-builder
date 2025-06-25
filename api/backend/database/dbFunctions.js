9f58eb6e9d7a5e64bd5c7b59186237a2b7f23260
shan <quijano.seanvenz@gmail.com>
2025-06-25 12:12:07 +0800

    added association

--- api/backend/database/dbFunctions.js
+++ api/backend/database/dbFunctions.js
@@ -1,12 +1,13 @@
 const sequelize = require('./sequelize');
-const License = require('../models/License')
+const License = require('../models/License');
+const defineAssociations = require('./associations');
 
 const initializeDatabase = async () => {
     try{
         await sequelize.authenticate();
         console.log('Database connection has been established successfully.');
 
-        // defineAssociations();
+        defineAssociations();
 
         await sequelize.sync({alter: false});
         console.log('Database synchronized successfully.');
